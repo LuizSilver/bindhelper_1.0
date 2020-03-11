757257f896bfc5b7cadc2a9059d05eddf00d88b1
Luiz_Silveira <nasciluiz2002@hotmail.com>
2020-03-11 09:11:11 -0300

    Update bindhelper2.sh

--- bindhelper2.sh
+++ bindhelper2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $$ >> /root/bindhelper.lock 
-OPC=$(dialog --backtitle 'BindHelper 1.0' --title 'Zone Options' --stdout --cancel-label 'Voltar' --menu 'Select a Function:' 0 0 0 "Create Zone" '' "Delete Zone" '' "Visualize Zones" '') 
+OPC=$(dialog --backtitle 'BindHelper 1.0' --title 'Zone Options' --stdout --cancel-label 'Voltar' --menu 'Select a function:' 0 0 0 "Create Zone" '' "Delete Zone" '' "Visualize Zones" '') 
 [[ $? -ne '0' ]] && /bin/bindhelper
-[[ $OPC == "Criar Zona" ]] && OPC2=$(dialog --stdout --menu 'Selecione o tipo:' 0 0 0 "Zona Direta" '' "Zona Reversa" '') && [[ $OPC2 == "Zona Direta" ]] && /etc/bindhelper/zone.sh || [[ $OPC2 == "Zona Reversa" ]] && /etc/bindhelper/enoz.sh 
-[[ $OPC == "Deletar Zona" ]] && /etc/bindhelper/rzone.sh 
-[[ $OPC == "Visualizar Zonas" ]] && /etc/bindhelper/view.sh
+[[ $OPC == "Create Zone" ]] && OPC2=$(dialog --stdout --menu 'Select the type:' 0 0 0 "Forward Zone" '' "Reverse Zone" '') && [[ $OPC2 == "Forward Zone" ]] && /etc/bindhelper/zone.sh || [[ $OPC2 == "Reverse Zone" ]] && /etc/bindhelper/enoz.sh 
+[[ $OPC == "Delete Zone" ]] && /etc/bindhelper/rzone.sh 
+[[ $OPC == "Visualize Zones" ]] && /etc/bindhelper/view.sh
