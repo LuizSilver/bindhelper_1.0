eded5a54366ac740939a99f6afb87b77a1dc1052
Luiz_Silveira <nasciluiz2002@hotmail.com>
2020-03-11 09:14:16 -0300

    Update bindhelper2.sh

--- bindhelper2.sh
+++ bindhelper2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $$ >> /root/bindhelper.lock 
-OPC=$(dialog --backtitle 'BindHelper 1.0' --title 'Zone Options' --stdout --cancel-label 'Voltar' --menu 'Select a function:' 0 0 0 "Create Zone" '' "Delete Zone" '' "Visualize Zones" '') 
+OPC=$(dialog --backtitle 'BindHelper 1.0' --title 'Zone Options' --stdout --cancel-label 'Back' --menu 'Select a function:' 0 0 0 "Create Zone" '' "Delete Zone" '' "Visualize Zones" '') 
 [[ $? -ne '0' ]] && /bin/bindhelper
 [[ $OPC == "Create Zone" ]] && OPC2=$(dialog --stdout --menu 'Select the type:' 0 0 0 "Forward Zone" '' "Reverse Zone" '') && [[ $OPC2 == "Forward Zone" ]] && /etc/bindhelper/zone.sh || [[ $OPC2 == "Reverse Zone" ]] && /etc/bindhelper/enoz.sh 
 [[ $OPC == "Delete Zone" ]] && /etc/bindhelper/rzone.sh 
